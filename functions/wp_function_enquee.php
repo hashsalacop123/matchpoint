896e51df3b64bd47269e8df731d3f54b5cb33c71
hashglobal25 <hashglobal25@gmail.com>
2026-02-13 15:41:04 +0800

    update enquee

--- functions/wp_function_enquee.php
+++ functions/wp_function_enquee.php
@@ -27,10 +27,10 @@ function theme_enqueue_styles() {
     wp_enqueue_style('fancy-box','https://cdn.jsdelivr.net/npm/@fancyapps/ui@6.1/dist/fancybox/fancybox.css');
 
     // Theme CSS
-    wp_enqueue_style('landing-page', get_stylesheet_directory_uri() . '/css/home-page.css');
-    wp_enqueue_style('innerpages', get_stylesheet_directory_uri() . '/css/inner-pages.css');
-    wp_enqueue_style('global-css', get_stylesheet_directory_uri() . '/css/style.css');
-    wp_enqueue_style($parent_style, get_stylesheet_directory_uri() . '/style.css');
+    wp_enqueue_style('landing-page', get_template_directory_uri() . '/css/home-page.css');
+    wp_enqueue_style('innerpages', get_template_directory_uri() . '/css/inner-pages.css');
+    wp_enqueue_style('global-css', get_template_directory_uri() . '/css/style.css');
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
 
     // jQuery
     wp_enqueue_script('jquery');
